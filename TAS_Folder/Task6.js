ad1e49de552f21d212d9014cae2c67ca283ee4b2
micheal-babalola <babalolamicheal1991@gmail.com>
2024-02-03 20:12:56 +0000

    Task6

---
+++ TAS_Folder/Task6.js
@@ -0,0 +1,25 @@
+//Case1
+// Create a variable number with a value of 10
+const number = 10;
+
+// Create an if statement to print to console odd or even number.
+if (number%2===0 || number1%2===0){
+    console.log("This is an Even Number")
+}
+else {
+    console.log("This is an Odd Number")
+}
+
+
+//Case2
+// Create a variable number1 to Take input from user 
+// let number1 = prompt("Please enter a Number: ");
+
+// // Create an if statement to print to console odd or even number.
+// if (number1%2===0){
+//     console.log("This is an Even Number")
+// }
+// else {
+//     console.log("This is an Odd Number")
+// }
+
